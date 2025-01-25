5c2a04a28a9805b2cfb4d27ab5ba6de6d865031f
Diego Revenga González <diegorevengagonzalez@gmail.com>
2025-01-25 17:29:25 +0100

    Avoid placing blocks on top of entities and other blocks

--- src/main/java/com/reven02/the_shuffle_wand/item/custom/ShuffleWandItem.java
+++ src/main/java/com/reven02/the_shuffle_wand/item/custom/ShuffleWandItem.java
@@ -2,7 +2,9 @@ package com.reven02.the_shuffle_wand.item.custom;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.util.ActionResult;
@@ -22,10 +24,25 @@ public class ShuffleWandItem extends Item {
 
         World world = context.getWorld();
 
-        if (!world.isClient) {
-            Block block = Blocks.AMETHYST_BLOCK;
+        Block block = Blocks.AMETHYST_BLOCK;
 
-            world.setBlockState(placingPos, block.getDefaultState());
+        // Check entity collisions
+        if (!world.canPlace(block.getDefaultState(), placingPos, ShapeContext.absent())) {
+            return ActionResult.FAIL;
+        }
+
+        // Check is not overriding blocks
+        ItemPlacementContext itemPlacementContext = new ItemPlacementContext(context);
+        if (!itemPlacementContext.canPlace()) {
+            return ActionResult.FAIL;
+        }
+
+        // Place block
+        // Both in client and server to avoid de-sync
+        world.setBlockState(placingPos, block.getDefaultState());
+
+        // ***** Only-Server context *****
+        if (!world.isClient) {
             world.playSound(null, placingPos, block.getDefaultState().getSoundGroup().getPlaceSound(), SoundCategory.BLOCKS);
         }
 
